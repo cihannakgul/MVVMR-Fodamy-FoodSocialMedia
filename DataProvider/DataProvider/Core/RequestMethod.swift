65daca3c2553b88d2c9c5c6004aac2a6734f0e6e
Cihan <cihannakgul@gmail.com>
2023-06-02 16:49:12 +0300

    data provider core contracts

---
+++ DataProvider/DataProvider/Core/RequestMethod.swift
@@ -0,0 +1,18 @@
+//
+//  RequestMethod.swift
+//  DataProvider
+//
+//  Created by cihan on 25.04.23.
+//
+
+public enum RequestMethod { // Enum of the RequestMethod
+    case connect
+    case delete
+    case get
+    case head
+    case options
+    case patch
+    case post
+    case put
+    case trace
+}
