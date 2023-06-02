37dbeed0fae48d4d7e187a6e5aa225e1d8a9bb13
Cihan <cihannakgul@gmail.com>
2023-06-02 16:58:50 +0300

    Constants for Closures, DefaultKeys and keyboardhelper

---
+++ Utilities/Utilities/Constants/DefaultsKeys.swift
@@ -0,0 +1,16 @@
+//
+//  DefaultsKeys.swift
+//  Utilities
+//
+//  Created by cihan on 27.03.23.
+//
+
+import Foundation
+
+
+import MobilliumUserDefaults
+
+public extension DefaultsKey {
+    static let isIntroNotComplete = Key<Bool>(key: "isIntroCompleted")
+    static let userId = Key<Int>(key: "userId")
+ }
