4eaf26aec1a36ad4b721295ec9b1ad0e12f46517
Cihan <cihannakgul@gmail.com>
2023-06-02 17:11:04 +0300

    data provider codable models for recipe

---
+++ DataProvider/DataProvider/Model/Recipe/NumberOfPerson.swift
@@ -0,0 +1,11 @@
+//
+//  NumberOfPerson.swift
+//  DataProvider
+//
+//  Created by cihan on 31.05.23.
+//
+
+ 
+public struct NumberOfPerson: Decodable {
+    public let text: String?
+}
---
+++ DataProvider/DataProvider/Model/Recipe/RecipeDetail.swift
@@ -0,0 +1,42 @@
+//
+//  RecipeDetail.swift
+//  DataProvider
+//
+//  Created by cihan on 31.05.23.
+//
+
+ 
+
+public struct RecipeDetail: Decodable {
+    public let id: Int
+    public let title: String?
+    public let ingredients: String?
+    public let instructions: String?
+    public let isLiked: Bool
+    public let timeDifference: String?
+    public let isEditorChoice: Bool
+    public let likeCount: Int
+    public let commentCount: Int
+    public let user: User
+    public let timeOfRecipe: RecipeTime
+    public let numberOfPerson: NumberOfPerson
+    public let category: CategoryDetail
+    public let images: [Image]
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case ingredients
+        case instructions = "directions"
+        case isLiked = "is_liked"
+        case timeDifference = "difference"
+        case isEditorChoice = "is_editor_choice"
+        case likeCount = "like_count"
+        case commentCount = "comment_count"
+        case user
+        case timeOfRecipe = "time_of_recipe"
+        case numberOfPerson = "number_of_person"
+        case category
+        case images
+    }
+}
