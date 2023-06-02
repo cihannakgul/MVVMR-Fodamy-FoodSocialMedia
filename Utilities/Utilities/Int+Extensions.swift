e791bd8ebbb499ef28872579246d7be05aa7a907
Cihan <cihannakgul@gmail.com>
2023-06-02 16:59:13 +0300

    Extensions for UICollectionView, variables and UIImage

---
+++ Utilities/Utilities/Int+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Int+Extensions.swift
+//  Utilities
+//
+//  Created by cihan on 31.05.23.
+//
+
+ public extension Int {
+    
+    var toString: String {
+        return String(self)
+    }
+    
+}
